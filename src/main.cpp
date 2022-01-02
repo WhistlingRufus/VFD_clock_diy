173c76ac351a311fefbdf999a021f6d639512642
WhistlingRufus <pugachevshina97@gmail.com>
2022-01-03 00:12:24 +0300

    seconds blink

--- src/main.cpp
+++ src/main.cpp
@@ -3,24 +3,30 @@
 #include <RTClib.h>
 #include <SPI.h>
 
-#define DELAY_VALUE 500
+#define DELAY_VALUE  3.5
 #define sda_clock PB_11
 #define scl_clock PB_10
+
+#define __DATE__1 "Jan  2 2022"
+#define __TIME__1 "22:51:52"
 
 
 const uint8_t segms[] = {PB_3, PB_4, PB_5, PB_6, PB_7, PB_8, PB_9, PB_0, PB_1}; //segms
 const uint8_t digs[] = {PA_0, PA_1, PA_2, PA_3, PA_4, PA_5, PA_6, PA_7};        //parts
 const char digits_masks[] = {0b000111111, 0b000001010, 0b001110011,
                              0b001101011, 0b001001110, 0b001101101,
-                             0b001111101, 0b000001011, 0b001111111, 0b001101111, 0b00000000}; //digits 0-9 + nothing
-const int days_of_week[] = {2, 3, 4, 5, 6, 7, 1};
+                             0b001111101, 0b000001011, 0b001111111, 0b001101111, 0b00000000,0b11111111}; //digits 0-9 + nothing+ seconds 
+const int days_of_week[] = {1,2, 3, 4, 5, 6, 7};
 TwoWire *i2c_clock = new TwoWire(sda_clock,scl_clock);
 RTC_DS3231 rtc;
 int day_ind = 0;
 int hour = 0;
-int minute = 0;
-DateTime now ;
+int minute =  0;
 
+
+DateTime now ;
+uint32_t time_second = millis();
+bool opt = false;
 void show_mask(char d, bool day_week = false)
 {
   for (int i = 0; i < sizeof(segms) / sizeof(*segms); i++)
@@ -49,9 +55,12 @@ void activate_dig(int dig)
   {
     if (i == dig)
       digitalWrite(digs[i], LOW);
+      
+          
     else
       digitalWrite(digs[i], HIGH);
-  }
+      
+        }
 }
 void draw_digit(int *time, int day)
 {
@@ -68,15 +77,28 @@ void draw_digit(int *time, int day)
       show_mask(digits_masks[2], disp_day);
     else if (d == 0)
       show_mask(digits_masks[1]); //выкл
-    else if (d == 4)
-      show_mask(digits_masks[10], disp_day);
+    else if (d == 4){
+      uint32_t delta = millis() - time_second;
+      if (delta  > 1000){
+        time_second =  millis();
+        opt=!opt;
+      }
+      if (opt)
+        show_mask(digits_masks[11], disp_day);
+      else
+        show_mask(digits_masks[10], disp_day);
+      
+      
+      
+    }
     else if (d == 1)
       show_mask(digits_masks[10], disp_day);
+    delay(DELAY_VALUE);
     clear_segs();
   }
 }
 void setup()
-{
+{ 
    //Serial.begin(9600);
   for (int i = 0; i < sizeof(segms) / sizeof(*segms); i++)
   {
@@ -90,7 +112,7 @@ void setup()
   }
   clear_segs();
   clear_digs();
-  if (! rtc.begin(i2c_clock)) 
+  if (!rtc.begin(i2c_clock)) 
     while (1) {
     activate_dig(0);
     digitalWrite(segms[5], LOW);
@@ -98,8 +120,7 @@ void setup()
   }
   //Serial.print("Is Clock Running: ");
    
-    
-  rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  rtc.adjust(DateTime(F(__DATE__1), F(__TIME__1)));
 
    
    
@@ -109,10 +130,11 @@ void setup()
 void loop()
 {
   now = rtc.now();
-  day_ind =now.dayOfTheWeek();
+  day_ind = now.dayOfTheWeek();
   hour = now.hour();
   minute = now.minute();
+  
   int time[] = {hour/10,hour%10,minute/10,minute%10};    
-  draw_digit(time,now.dayOfTheWeek()-1);
+  draw_digit(time,day_ind);
   
 }
